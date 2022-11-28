677d3b8e3cf275bcfaf92d8f9abd36d58b0bbaf1
SofiaDivine <sonimavispc@gmail.com>
2022-11-28 14:49:03 +0200

    Added days data

--- index.js
+++ index.js
@@ -31,18 +31,18 @@ function displayForecast(response) {
         forecastHTML +
         `
       <div class="col-2">
-
+<div class="weather-forecast-date">${forecastDay.dt}</div>
         <img
           src="http://openweathermap.org/img/wn/${
             forecastDay.weather[0].icon
           }@2x.png"
           alt=""
-          width="42"
+          width="50"
         />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> ${Math.round(
+          <span class="weather-forecast-temperature-max"> <strong> ${Math.round(
             forecastDay.temp.max
-          )}° </span>
+          )}° </strong> </span>
           <span class="weather-forecast-temperature-min"> ${Math.round(
             forecastDay.temp.min
           )}° </span>
